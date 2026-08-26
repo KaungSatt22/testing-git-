0a6f84790d274280f4f6ad9055fe5f9844095dec
K <kaungsatt.2k1@gmail.com>
2026-08-26 15:55:36 +0630

    aa

--- test.js
+++ test.js
@@ -1,36 +1,21 @@
-const { jsx } = require("react/jsx-runtime");
-
 let name = "smh";
 let efg = "efg";
 let abc = "abc";
 let mmm = "mmm";
 mmm = "mmm";
 mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
-mmm = "mmm";
+mmm = "qwertyuiop";
 mmm = "mmm";
 mmm = "mmm";
 mmm = "mmm";
 
-a;
-b;
-c;
-d;
-e;
-f;
-g;
-h;
-i;
-j;
-k;
-l;
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
+mmm = "mmm";
