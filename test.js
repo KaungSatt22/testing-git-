f8fa987f573cbb3db888beba6724e0d284822bed
SMH-TAT <sitminhtet@thuriyaacetechnology.com>
2026-08-26 15:54:33 +0630

    test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const { jsx } = require("react/jsx-runtime");
+
 let name = "smh";
 let efg = "efg";
 let abc = "abc";
@@ -20,3 +22,15 @@ mmm = "mmm";
 mmm = "mmm";
 mmm = "mmm";
 
+a;
+b;
+c;
+d;
+e;
+f;
+g;
+h;
+i;
+j;
+k;
+l;
